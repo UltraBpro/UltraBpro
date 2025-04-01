d1fbede17d1a10ca203e84963f745a098ba798f2
SuperiorBpro <b3bpro123@gmail.com>
2025-04-01 09:50:51 +0700

    Cool update

--- ultrabpro-portfolio/src/App.jsx
+++ ultrabpro-portfolio/src/App.jsx
@@ -344,7 +344,7 @@ function App() {
             <div className="character-intro">
               <div className="character-avatar">
                 <img
-                  src="102210199-lower.jpg"
+                  src={secretModeActive?"102210199-lower.jpg":"102210199-cooler.jpg"}
                   alt="Character Avatar"
                 />
               </div>
